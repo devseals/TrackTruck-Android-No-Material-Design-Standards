eecd0389241813971c63bb7d25bb23c012afceaa
Kenzo Van Waerebeek Alfaro <32316281+KenzoVWA@users.noreply.github.com>
2019-04-08 13:54:46 -0500

    Initial commit

---
+++ app/src/main/java/pe/edu/upc/activities/FoodTrucksList.kt
@@ -0,0 +1,15 @@
+package pe.edu.upc.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pe.edu.upc.R
+
+class FoodTrucksListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food_trucks_list)
+
+    }
+
+}
